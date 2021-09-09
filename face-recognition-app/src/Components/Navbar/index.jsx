d2f9614a90445c6b97be98f946f1f3e932e08de1
ofoezie ezichi favour <ezichifavour@gmail.com>
2021-09-09 12:57:31 +0100

    Scoped styling on navbar

--- face-recognition-app/src/Components/Navbar/index.jsx
+++ face-recognition-app/src/Components/Navbar/index.jsx
@@ -3,42 +3,32 @@ import { Link } from 'react-router-dom';
 import logo from '../../store/logo.svg';
 import logo2 from '../../store/logo2.svg';
 import ss from '../../store/ss.svg';
-import './Navbar.css';
+import classes from './Navbar.module.css';
 //import {Router} from 'react-router-dom';
 
 function Navbar() {
     return (
 
-        // <header className={classes.header}>
+        <header className={classes.header}>
 
-        <div className="navbar">
-            <nav className="nav-content">
-                {/* <div className="nav-content-images">
-                    <img src={logo2} alt="" />
-                    <img src={logo} alt="" />
-                    <br></br>
-                    <img src={ss} alt="" />
-                </div> */}
-                <ul>
-
-                    <li>
-                        <Link to='/' >Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/services' >Services</Link>
-                    </li>
-                    <li>
-                        <Link to='/Login' >Login</Link>
-                    </li>
-                    <li>
-                        <Link to='/sign-up' >Sign Up</Link>
-                    </li>
-                </ul>
-
+        <div className={classes.navbar}>
+                     <nav>
+                    
+                     <img className= {classes.content} src={logo2} alt="" />
+                     <img className= {classes.content2} src={logo} alt="" />
+                     <br></br>
+                 <img className= {classes.content3} src={ss} alt="" />
+              
+               
+                 <ul>
+                    <li><Link to='/' >Home</Link></li>
+                    <li><Link to='/services' >Services</Link></li>
+                    <li> <Link to='/Login' >Login</Link></li>
+                    <li><Link to='/sign-up' >Sign Up</Link></li> </ul>
             </nav>
 
         </div>
-        // </header>
+        </header>
 
     );
 }
