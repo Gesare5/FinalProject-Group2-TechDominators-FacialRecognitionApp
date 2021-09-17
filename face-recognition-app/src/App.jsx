c465b1547bdeb8c398e68b71274efd1d1a10e7ef
Hafziyah <hafsatajia@gmail.com>
2021-09-17 06:23:36 +0100

    worked on the index file of the services page

--- face-recognition-app/src/App.jsx
+++ face-recognition-app/src/App.jsx
@@ -1,42 +1,35 @@
 import Navbar from "./Components/Navbar";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Landing from "./Pages/Landing";
 import Login from "./Pages/Login";
 import SignUp from "./Pages/SignUp";
 import ContactUs from "./Pages/Contact-us";
-
-
+import Services from "./Pages/Services";
 
 function App() {
   return (
-
-
     <div className="App">
-
       <BrowserRouter>
         <Navbar />
         <Switch>
-          <Route path='/' exact>
+          <Route path="/" exact>
             <Landing />
           </Route>
-          {/* <Route path='/services'>
-            <Services /> */}
-          {/* </Route> */}
-          <Route path='/contactus'>
+          <Route path="/services">
+            <Services />
+          </Route>
+          <Route path="/contactus">
             <ContactUs />
           </Route>
           <Route path="/Login">
             <Login />
           </Route>
-          <Route path='/sign-up'>
+          <Route path="/sign-up">
             <SignUp />
           </Route>
         </Switch>
-
       </BrowserRouter>
-
     </div>
-
   );
 }
 
